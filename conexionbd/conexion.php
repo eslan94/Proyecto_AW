a2f496082ef60f18bbb8f08101d1bf4d1d68a61a
eslan94 <eslan94.eca@gmail.com>
2024-01-19 02:12:55 -0500

    conexionbd

---
+++ conexionbd/conexion.php
@@ -0,0 +1,9 @@
+<?php
+    $db_host='localhost';
+    $db_usuario='root';
+    $db_password='';
+    $db_database='proyecto_aw';
+
+    $conexion=mysqli_connect($db_host, $db_usuario, $db_password, $db_database);
+
+?>
---
+++ conexionbd/registro.php
@@ -0,0 +1,20 @@
+<?php
+    include 'conexion.php';
+
+    $nombre=$_POST['nombre'];
+    $apellido=$_POST['apellido'];
+    $email=$_POST['email'];
+    $celular=$_POST['celular'];
+    $direccion=$_POST['direccion'];
+
+    $consulta="INSERT into usuario(nombre, apellido, email, celular, direccion) values('$nombre', '$apellido', '$email', '$celular', '$direccion')";
+
+    $resultado=mysqli_query($conexion, $consulta);
+
+    if($resultado){
+        echo "Registro exitoso";
+    }else{
+        echo "Error de registro";
+    }
+    
+?>
